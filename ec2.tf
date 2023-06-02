9884c7e84a01fceba095117f56d6aa3e0f9b9bec
kushagra29120 <132610593+kushagra29120@users.noreply.github.com>
2023-06-03 01:44:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,5 @@
 resource "aws_instance" "myec2" {
 
 ami = "ami-02396cdd13e9a1257"
-instance_type = local.instance_type
-
-}
-
-locals {
-
-instance_type = "t2.large"
-}
-
-output "ins_id" {
-
-value = aws_instance.myec2.id
-
+instance_type = "t2.micro"
 }
